ed50fa30495856ee630f4056aaa3397cb8903357
rodrigofdl <rodrigo.fdlira@gmail.com>
2023-12-05 17:31:06 -0300

    First commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,43 @@
+package calculator;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener {
+
+    JFrame frame;
+    JTextField textfield;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    JPanel panel;
+
+    Font myFont = new Font("Arial", Font.BOLD, 30);
+
+    double num1=0,num2=0,result=0;
+    char operator;
+
+    Calculator() {
+
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+
+
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
